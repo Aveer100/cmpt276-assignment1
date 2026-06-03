71f9b005fb0bc5bef3a6cce267c6311134f17c8c
Aveer100 <ass96@sfu.ca>
2026-06-02 20:09:13 -0700

    setting up

---
+++ src/test/java/assignment1/weather_site/WeatherSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment1.weather_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
